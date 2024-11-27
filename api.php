3517e44729ab430c1c7dc507f98acd02ab408365
yadirahm150905 <1555291@senati.pe>
2024-11-27 13:44:30 -0500

    27/11/2024

--- api.php
+++ api.php
@@ -1,16 +1,28 @@
 <?php
+//nombre: YADIRA YAHAIRA HUAMAN MEZA
+
 header("Access-Control-Allow-Origin: *");
+
 $metodo = $_SERVER['REQUEST_METHOD'];
 
 $respuesta =[];
 switch ($metodo) {
     case 'GET':
-        $respuesta =['mensaje'=>'Metodo Get Correcto ',
-        
+        $respuesta =[
+        'mensaje'=>'Metodo Get Correcto ',       
         'data'=> $_GET
     ];# code...
 
         break;
+    
+    case 'POST':
+     $data_entrante = json_decode(file_get_contents("php://input"),true);  // json_encode( => convertir en json      json_decode=> convertir a array     ->el codigo captura la data body
+
+        $respuesta =[
+        'mensaje'=>'Metodo POST de retorno ',       
+        'data'=> $data_entrante,
+
+    ];# code...
     
     default:
         # code...
@@ -18,3 +30,24 @@ switch ($metodo) {
 }
 
 echo json_encode($respuesta);
+
+//////////////////////////////////////////////////////
+
+// nombre: YADIRA YAHAIRA HUAMAN MEZA
+
+header("Access-Control-Allow-Origin: *");
+
+$metodo1 = $_SERVER['REQUEST_METHOD'];
+
+$respuesta1 =[];
+switch ($metodo1) {
+    case 'GET':
+        $respuesta =[
+        'mensaje'=>'Metodo Get Correcto ',       
+        'data'=> $_GET
+    ];# code...
+
+        break;
+}
+echo json_encode($respuesta);
+
--- app.js
+++ app.js
@@ -21,7 +21,7 @@ botonGet.addEventListener('click',function() {
 
 });
 */
-
+// Nombre: YADIRA YAHAIRA HUAMAN MEZA
 const apiUrl = "http://localhost/app-php-js-chaat-yes-no-senati/api.php";
 
 async function getData() {
@@ -40,5 +40,67 @@ async function getData() {
 
 let botonGet = document.querySelector('#getdata'); // Cambié getSelection por querySelector
 botonGet.addEventListener('click', function () {
+    getData();
+});
+
+async function postData() {
+    console.log('Ingreso al POST Data');
+    try {
+        const  respuesta = await fetch(apiUrl, {
+             method: 'POST',
+             headers:{
+                "Content-Type": "application/json"
+             },
+             body: JSON.stringify({
+                nombre:'Yadira',
+                apellido:'Huaman',
+                lenguaje_favorito: 'JavaScript',
+                color : 'rojo'
+             })            
+             
+        });
+const data_retorna = await respuesta.json();
+console.log(data_retorna);
+
+    } catch (error) {
+        console.log("Error al momento de hacer la peticon POST:",error);
+    }
+    //alert('ingresoaquiiiiiiiiiiiiii')
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+let botonPost = document.querySelector('#post-data');
+botonPost.addEventListener('click', function() {
+    postData();
+//alert ('ingreso aquiii');
+
+
+});
+
+
+//apuntado a la url 
+//con get a 
+//https://yesno.wtf/api
+
+//nombre :Yadira Yahaira Huaman Meza
+
+const apiUrl2 = "https://yesno.wtf/api";
+
+async function getData() {
+    console.log('Ingreso al get Data');
+    try {
+        const respuesta = await fetch(apiUrl2, {
+            method: 'GET',
+        });
+
+        const data = await respuesta.json();
+        console.log(data);
+    } catch (error) {
+        console.log("Error al momento de hacer la petición GET:", error);
+    }
+}
+
+let botonGet2 = document.querySelector('#getURL'); 
+botonGet2.addEventListener('click', function () {
     getData();
 });
