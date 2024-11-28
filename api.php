e4cf40ca2af3ade5e6b25c8f2bc2898be9a54c08
yadirahm150905 <1555291@senati.pe>
2024-11-28 13:51:03 -0500

    28/11/2024

--- api.php
+++ api.php
@@ -1,53 +1,27 @@
 <?php
-//nombre: YADIRA YAHAIRA HUAMAN MEZA
-
+//aqui
 header("Access-Control-Allow-Origin: *");
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 
-$respuesta =[];
+$respuesta = [];
 switch ($metodo) {
     case 'GET':
-        $respuesta =[
-        'mensaje'=>'Metodo Get Correcto ',       
-        'data'=> $_GET
-    ];# code...
-
+        $respuesta = [
+            'mensaje' => 'Metodo Get Correcto',
+            'data' => $_GET
+        ];
         break;
-    
     case 'POST':
-     $data_entrante = json_decode(file_get_contents("php://input"),true);  // json_encode( => convertir en json      json_decode=> convertir a array     ->el codigo captura la data body
+        $data_entrate = json_decode(file_get_contents("php://input"),true);
+        $respuesta = [
+            'mensaje' => 'Metodo Post de retorno',
+            'data' => $data_entrate,
+        ];
+        break;
 
-        $respuesta =[
-        'mensaje'=>'Metodo POST de retorno ',       
-        'data'=> $data_entrante,
-
-    ];# code...
-    
     default:
         # code...
         break;
 }
-
 echo json_encode($respuesta);
-
-//////////////////////////////////////////////////////
-
-// nombre: YADIRA YAHAIRA HUAMAN MEZA
-
-header("Access-Control-Allow-Origin: *");
-
-$metodo1 = $_SERVER['REQUEST_METHOD'];
-
-$respuesta1 =[];
-switch ($metodo1) {
-    case 'GET':
-        $respuesta =[
-        'mensaje'=>'Metodo Get Correcto ',       
-        'data'=> $_GET
-    ];# code...
-
-        break;
-}
-echo json_encode($respuesta);
-
--- app.js
+++ app.js
@@ -22,85 +22,110 @@ botonGet.addEventListener('click',function() {
 });
 */
 // Nombre: YADIRA YAHAIRA HUAMAN MEZA
-const apiUrl = "http://localhost/app-php-js-chaat-yes-no-senati/api.php";
+const apiUrl = "http://localhost/app-php-js-chat-yes-no-senati/api.php";
+const apiUrlYesNo = "https://yesno.wtf/api";
 
 async function getData() {
-    console.log('Ingreso al get Data');
-    try {
-        const respuesta = await fetch(`${apiUrl}?id=123&nombre=Yadira&apellido=HuamanMeza`, {
-            method: 'GET',
-        });
-
-        const data = await respuesta.json();
-        console.log(data);
-    } catch (error) {
-        console.log("Error al momento de hacer la petición GET:", error);
-    }
+  console.log("ingreso a get data");
+  try {
+    const respuesta = await fetch(
+      `${apiUrl}?id=123&nombre=Diego&apellido=Lipa`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await respuesta.json();
+    console.log("data");
+    console.log(data);
+  } catch (error) {
+    console.log("Error al momento de hacer la peticion GET: ", error);
+  }
 }
 
-let botonGet = document.querySelector('#getdata'); // Cambié getSelection por querySelector
-botonGet.addEventListener('click', function () {
-    getData();
+let botonGet = document.querySelector(".getdata");
+botonGet.addEventListener("click", function () {
+  getData();
 });
 
 async function postData() {
-    console.log('Ingreso al POST Data');
-    try {
-        const  respuesta = await fetch(apiUrl, {
-             method: 'POST',
-             headers:{
-                "Content-Type": "application/json"
-             },
-             body: JSON.stringify({
-                nombre:'Yadira',
-                apellido:'Huaman',
-                lenguaje_favorito: 'JavaScript',
-                color : 'rojo'
-             })            
-             
-        });
-const data_retorna = await respuesta.json();
-console.log(data_retorna);
-
-    } catch (error) {
-        console.log("Error al momento de hacer la peticon POST:",error);
-    }
-    //alert('ingresoaquiiiiiiiiiiiiii')
+  try {
+    const respuesta = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: "Yadira",
+        apellido: "Huaman",
+        lenguaje_favorito: "JavaScript, Pyton",
+        color: "Rojo",
+      }),
+    });
+    const data_retorno = await respuesta.json();
+    console.log(data_retorno);
+  } catch (error) {
+    console.log("Error al momento de hace la peticion POST: ", error);
+  }
 }
-//////////////////////////////////////////////////////////////////////////////////////////////
 
-let botonPost = document.querySelector('#post-data');
-botonPost.addEventListener('click', function() {
-    postData();
-//alert ('ingreso aquiii');
+let botonPost = document.querySelector("#post-data");
+botonPost.addEventListener("click", function () {
+  postData();
+});
+
+async function getYesNoData() {
+  try {
+    const respuesta = await fetch(apiUrlYesNo,
+      {
+        method: "GET",
+      }
+    );
+    const data = await respuesta.json();
+    console.log('Aqui llega la información');
+    console.log(data);
+    agregarMensaje(data.answer,false,data.image);
 
 
+  } catch (error) {
+    console.log("Error al momento de hacer la peticion GET: ", error);
+  }
+}
+
+let botonGetYesNo = document.querySelector("#yes-no-data");
+botonGetYesNo.addEventListener("click", function () {
+    getYesNoData();
 });
 
 
-//apuntado a la url 
-//con get a 
-//https://yesno.wtf/api
+// Funcionalidad del chat yes no
+let chatMessages = document.getElementById('chatMessages'); 
+let chatForm = document.getElementById('chatForm');
+let messageInput = document.getElementById('messageInput');
 
-//nombre :Yadira Yahaira Huaman Meza
+function agregarMensaje(mensaje, soyYo = true, imagen = null){
+  const mensajeDiv = document.createElement('div');
+  mensajeDiv.classList.add('message');
+  mensajeDiv.classList.add(soyYo? 'user-message':'api-message');
+  mensajeDiv.textContent = mensaje;
 
-const apiUrl2 = "https://yesno.wtf/api";
+  if(imagen){
+    const img = document.createElement('img');
+    img.src = imagen;
+    mensajeDiv.appendChild(img);
+  }
 
-async function getData() {
-    console.log('Ingreso al get Data');
-    try {
-        const respuesta = await fetch(apiUrl2, {
-            method: 'GET',
-        });
+  setTimeout(() => {
+    chatMessages.scrollTop = chatMessages.scrollHeight
+  }, 500);
 
-        const data = await respuesta.json();
-        console.log(data);
-    } catch (error) {
-        console.log("Error al momento de hacer la petición GET:", error);
-    }
+  chatMessages.appendChild(mensajeDiv);
 }
 
-let botonGet2 = document.querySelector('#getURL'); 
-botonGet2.addEventListener('click', function () {
-    getData();
+chatForm.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  const miMensaje = messageInput.value;
+  agregarMensaje(miMensaje,true);
+  
+  getYesNoData();
 });
